c9473eae5593bc244a029c1364dc810df574b39f
EC2 Default User <ec2-user@ip-172-31-36-151.ap-south-1.compute.internal>
2026-01-09 11:14:02 +0000

    update tf files v.4.1

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,11 +1,4 @@
 ################################
-# Availability Zones
-################################
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-################################
 # VPC
 ################################
 resource "aws_vpc" "main" {
