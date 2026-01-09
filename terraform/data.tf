0f0dbdef9db59368bdb2d46595839c3b91d71a5c
EC2 Default User <ec2-user@ip-172-31-36-151.ap-south-1.compute.internal>
2026-01-09 09:51:45 +0000

    Terraform updated v2

---
+++ terraform/data.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "al2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,37 @@
+################################
+# Availability Zones
+################################
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+################################
+# VPC
+################################
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.project}-vpc"
+  }
+}
+
+################################
+# Internet Gateway
+################################
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project}-igw"
+  }
+}
+
+################################
+# Public Subnet 1
+################################
 # tfsec:ignore:aws-ec2-no-public-ip-subnet
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
@@ -10,6 +44,9 @@ resource "aws_subnet" "public_1" {
   }
 }
 
+################################
+# Public Subnet 2
+################################
 # tfsec:ignore:aws-ec2-no-public-ip-subnet
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
@@ -20,5 +57,34 @@ resource "aws_subnet" "public_2" {
   tags = {
     Name = "${var.project}-public-2"
   }
+}
+
+################################
+# Route Table
+################################
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project}-public-rt"
+  }
+}
+
+################################
+# Route Table Associations
+################################
+resource "aws_route_table_association" "public_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public_2.id
+  route_table_id = aws_route_table.public.id
 }
 
