28c730822ab4e4b7f779337956c762413b217ac1
EC2 Default User <ec2-user@ip-172-31-36-151.ap-south-1.compute.internal>
2026-01-09 09:21:03 +0000

    Fix TFLint warnings: typed variables and provider constraints

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-south-1"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,28 +1,36 @@
 variable "project" {
+  type    = string
   default = "contact-book"
 }
 
 variable "vpc_cidr" {
+  type    = string
   default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr_1" {
+  type    = string
   default = "10.0.1.0/24"
 }
 
 variable "public_subnet_cidr_2" {
+  type    = string
   default = "10.0.2.0/24"
 }
 
 variable "instance_type" {
+  type    = string
   default = "t3.micro"
 }
 
 variable "db_username" {
+  type    = string
   default = "postgres"
 }
 
 variable "db_password" {
-  default = "postgres123"
+  type      = string
+  default   = "postgres123"
+  sensitive = true
 }
 
